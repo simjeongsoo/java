19805901af01ca115ae1e9d9c5629170e6fdb5ef
simjeongsoo <simking95@naver.com>
2022-05-13 20:44:53 +0900

    ch01

---
+++ Chapter01/src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public int sum(int num1,int num2){
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+    public int sub(int num1,int num2){
+        int sub = 0;
+        sub = num1 - num2;
+        return sub;
+    }
+    public int mul(int num1, int num2){
+        return num1 * num2;
+    }
+    public int div(int num1, int num2){
+        int result = num1/num2;
+        return result;
+    }
+}
