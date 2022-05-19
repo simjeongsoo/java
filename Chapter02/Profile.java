a2b3f392704c5416b492ad77663e141a8885c553
simjeongsoo <simking95@naver.com>
2022-05-19 18:59:38 +0900

    ch02

---
+++ Chapter02/Profile.java
@@ -0,0 +1,8 @@
+public class Profile{
+  public static void main(String[] args){
+    String name = "Java";
+    int age = 10;
+    System.out.println("My name is " + name);
+    System.out.println("My age is " + age);
+  }
+}
