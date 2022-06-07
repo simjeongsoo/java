6066ab9c0b0445984cd8a8611581e91a794a1d81
simjeongsoo <simking95@naver.com>
2022-06-07 17:49:01 +0900

    ch11_api

---
+++ Chapter11_api/src/APICheck.java
@@ -0,0 +1,12 @@
+public class APICheck {
+    public static void main(String[] args) {
+        APICheck apiCheck = new APICheck();
+        apiCheck.useDeprecated();
+    }
+
+    public void useDeprecated() {
+        String str = "abcde";
+        byte[] strBytes = str.getBytes();
+        String convertedStr = new String(strBytes,0);
+    }
+}
