13feb790520c492e4b31d197667b047cf9ee7f0c
simjeongsoo <simking95@naver.com>
2022-06-20 12:16:11 +0900

    오타수정

--- Chapter17_annotation/src/c/annotation/UserAnnotation.java
+++ Chapter17_annotation/src/c/annotation/UserAnnotation.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD) // 해당 어노테이션 사용 대상 지정 -> 메소드에 사용
-@Retention(RetentionPolicy.RUNTIME) // 어노테이션 유지 정보 지정 -> 깅랜기네 니 어노테이션 참조
+@Retention(RetentionPolicy.RUNTIME) // 어노테이션 유지 정보 지정 -> 실행시에 이 어노테이션 참조
 public @interface UserAnnotation { // @interface 로 선언하면 @UserAnnotation 으로 어노테이션 사용 가능
     public int number(); // 리턴 타입 int 메소드
             // 어노테이션 안에 메소드를 선언해 놓으면 이 어노테이션을 사용할 때 해당 항목에 대한 타입으로 값 지정 가능
