a0bfa203cff93fe4dc4092b777cc3ffe28f32d56
simjeongsoo <simking95@naver.com>
2022-07-05 15:15:39 +0900

    ch26 I/O v1.0

---
+++ Chapter26_IO/src/e/io/FileManageClass.java
@@ -0,0 +1,45 @@
+package e.io;
+
+import java.io.File;
+import java.io.IOException;
+
+// File 클래스를 이용하여 파일 처리
+public class FileManageClass {
+    public static void main(String[] args) {
+        FileManageClass sample = new FileManageClass();
+        String pathName =
+                File.separator + "/Users"
+                        + File.separator + "/simjeongsu"
+                        + File.separator + "/Documents"
+                        + File.separator + "/GodOfJava/"
+                        + File.separator + "Chapter26_IO"
+                        + File.separator + "test";
+        String fileName = "test.text";
+        sample.checkFile(pathName, fileName);
+    }
+
+    // file.createNewFile()
+    public void checkFile(String pathName, String fileName) {
+        File file = new File(pathName, fileName);
+        try {
+            System.out.println("Create result = " + file.createNewFile());
+            getFileInfo(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 파일의 정보를 확인하는 메소드
+    public void getFileInfo(File file) throws IOException{
+        System.out.println("Absolute path = " + file.getAbsolutePath());
+        System.out.println("Absolute file = " + file.getAbsoluteFile());
+        System.out.println("Canonical path = " + file.getCanonicalPath());
+        System.out.println("Canonical file = " + file.getCanonicalFile());
+
+        System.out.println("Name = " + file.getName());
+        System.out.println("Path = " + file.getPath());
+
+        System.out.println("Parent = " + file.getParent()); // 파일명을 제외한 경로만 출력
+
+    }
+}
---
+++ Chapter26_IO/src/e/io/FileSample.java
@@ -0,0 +1,55 @@
+package e.io;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Date;
+
+// 파일의 경로와 관련된 메소드들을 사용
+public class FileSample {
+    public static void main(String[] args) {
+        FileSample sample = new FileSample();
+//        String pathName = File.separator +"/Users/simjeongsu/Documents/GodOfJava/Chapter26_IO";
+        String pathName = "/Users/simjeongsu/Documents/GodOfJava/Chapter26_IO/test";
+//        sample.checkPath(pathName);
+//        sample.makeDir(pathName);
+//        sample.checkFileMethods(pathName);
+//        sample.canFileMethods(pathName);
+        sample.lastModified(pathName);
+    }
+
+    // file.exists()
+    public void checkPath(String pathName) {
+        File file = new File(pathName); // 매개변수로 넘어온 경로로 File 객체 생성
+        System.out.println(pathName + " is exists? " + file.exists()); // exists() 메소드로 해당 경로 존재 확인
+
+//        System.out.println(file.getName());
+    }
+
+    // file.mkdir()
+    public void makeDir(String pathName) {
+        File file = new File(pathName);
+        System.out.println("Make "+pathName+" result = "+file.mkdir());
+    }
+
+    // file.isDirectory() , file.isFile() , file.isHidden()
+    public void checkFileMethods(String pathName) {
+        File file = new File(pathName);
+        System.out.println(pathName + " is directory? = " + file.isDirectory());
+        System.out.println(pathName + " is file? = " + file.isFile());
+        System.out.println(pathName + " is hidden? = " + file.isHidden());
+    }
+
+    // 권한 확인 메소드
+    public void canFileMethods(String pathName) {
+        File file = new File(pathName);
+        System.out.println(pathName + " can read? = " + file.canRead());
+        System.out.println(pathName + " can write? = " + file.canWrite());
+        System.out.println(pathName + " can Execute? = " + file.canExecute()); // Java 6
+    }
+
+    // 파일이나 경로가 언제 생성되었는지 확인하는 메소드
+    public void lastModified(String pathName) {
+        File file = new File(pathName);
+        System.out.println(pathName+" last modified = "+new Date(file.lastModified()));
+    }
+}
