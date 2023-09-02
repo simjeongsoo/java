d91ef1493f9e6fff4d5735ec3aa4544c76753186
simjeongsoo <simking95@naver.com>
2023-09-02 23:49:11 +0900

    [코드 7-1] 메모리 누수 찾기

---
+++ effective-java/src/Stack.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+// 메모리 누수가 일어나는 위치는 어디인가?
+//==간단한 스택 코드==//
+public class Stack {
+
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return elements[--size];
+    }
+
+    /**
+     * 원로를 위한 공간을 적어도 하나 이상 확보한다.
+     * 배열 크기를 늘려야 할 때마다 대략 두 배씩 늘린다.
+     */
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+}
