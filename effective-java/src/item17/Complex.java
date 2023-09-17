9ef9f94f4d98c272376fbb76c4aa58baa0c5165f
simjeongsoo <simking95@naver.com>
2023-09-17 19:19:09 +0900

    [코드 17-2] 생성자 대신 정적 팩터리를 사용한 불변 클래스

--- effective-java/src/item17/Complex.java
+++ effective-java/src/item17/Complex.java
@@ -7,9 +7,14 @@ public final class Complex {
     private final double re;
     private final double im;
 
-    public Complex(double re, double im) {
+    private Complex(double re, double im) {
         this.re = re;
         this.im = im;
+    }
+
+    // 생성자 대신 정적 팩터리를 사용한 불변 클래스 - 상속 불가(final 보다 유연함)
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
     }
 
     public double realPart() {
