aff67efa0497d7722eb8f597967bd88c12d6cebd
simjeongsoo <simking95@naver.com>
2023-09-24 17:49:21 +0900

    불변 클래스를 만드는 다섯가지 규칙 - 불변 클래스에는 상속을 방지하도록 final 을 사용해야 한다.

--- effective-java/src/item17/PhoneNumber.java
+++ effective-java/src/item17/PhoneNumber.java
@@ -1,6 +1,9 @@
 package item17;
 
-public class PhoneNumber {
+/**
+ * 불변 클래스에는 상속을 방지하도록 final 을 사용해야 한다.
+ */
+public final class PhoneNumber {
     private short areaCode, prefix, lineNum;
 
     // 생성자로 초기화된 객체는 변경할 수 없다.
