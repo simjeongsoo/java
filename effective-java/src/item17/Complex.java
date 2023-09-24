c0bdb64090f95dcfe2c73e9716ccf2d8fb679bf2
simjeongsoo <simking95@naver.com>
2023-09-24 19:07:21 +0900

    effectively-final class

--- effective-java/src/item17/Complex.java
+++ effective-java/src/item17/Complex.java
@@ -3,18 +3,29 @@ package item17;
 /**
  * 불변 복소수 클래스
  */
-public final class Complex {
+public class Complex {
     private final double re;
     private final double im;
+
+    // 같은 인스턴스라면 상수로 선언해서 사용할 수 도 있다.
+    public static final Complex ZERO = new Complex(0, 0);
+    public static final Complex ONE = new Complex(1, 0);
+    public static final Complex I = new Complex(0, 1);
 
     private Complex(double re, double im) {
         this.re = re;
         this.im = im;
     }
 
+    public static class MyComplex extends Complex{
+        private MyComplex(double re, double im) {
+            super(re, im);
+        }
+    }
+
     // 생성자 대신 정적 팩터리를 사용한 불변 클래스 - 상속 불가(final 보다 유연함)
     public static Complex valueOf(double re, double im) {
-        return new Complex(re, im);
+        return new MyComplex(re, im);
     }
 
     public double realPart() {
