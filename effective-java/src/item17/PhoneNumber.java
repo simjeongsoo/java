23e5830b578227f0fe647cf9f54912f0f972370c
simjeongsoo <simking95@naver.com>
2023-09-24 17:52:05 +0900

    불변 클래스를 만드는 다섯가지 규칙 - 모든 필드를 final로 선언한다. : 컴파일 단계에서 체크 가능

--- effective-java/src/item17/PhoneNumber.java
+++ effective-java/src/item17/PhoneNumber.java
@@ -4,6 +4,8 @@ package item17;
  * 불변 클래스에는 상속을 방지하도록 final 을 사용해야 한다.
  */
 public final class PhoneNumber {
+
+    // 모든 필드를 final로 선언한다. : 컴파일 단계에서 체크 가능
     private final short areaCode, prefix, lineNum;
 
     // 생성자로 초기화된 객체는 변경할 수 없다.
