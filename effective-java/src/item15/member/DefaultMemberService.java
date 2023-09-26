dba65c0ecf4924c059204bb899b763a4c72affd3
simjeongsoo <simking95@gmail.com>
2023-09-26 17:45:27 +0900

    내부 중첩 클래스의 접근 제한자

--- effective-java/src/item15/member/DefaultMemberService.java
+++ effective-java/src/item15/member/DefaultMemberService.java
@@ -1,5 +1,7 @@
 package item15.member;
 
+import java.util.Arrays;
+
 /**
  * MemberService 인터페이스를 구현한 DefaultMemberService는 package-private 으로 선언
  *  -> 구현체는 내부 패키지 안에서만 알면 되기 때문
@@ -8,10 +10,28 @@ package item15.member;
  *  -> 서비스 로딩을 한다고해도 인터페이스만 알면 된다.
  */
 class DefaultMemberService implements MemberService{
-    MemberRepository memberRepository;
 
-    public Member getMember() {
-        memberRepository.findById();
-        return null;
+    private String name;
+
+    // DefaultMemberService와 단방향으로 사용한다면 private static 사용
+    private static class PrivateStaticClass {
+        // inner class 이지만 DefaultMemberService 클래스를 참조하는 필드가 없다.
+        //  -> DefaultMemberService 클래스와 각각의 클래스로 정의됨
     }
+
+    // DefaultMemberService와 양방향으로 사용한다면 private 사용
+    private class PrivateClass {
+        // static이 아니라면 감싸고 있는 DefaultMemberService의 인스턴스를 가지고 있다.
+        //  -> DefaultMemberService 클래스의 필드에 접근하는 것이 수월하다.
+        void doSomething() {
+            System.out.println(name);
+        }
+    }
+
+    public static void main(String[] args) {
+        // DefaultMemberService 타입으로 필드가 선언되어있어 서로 참조 가능
+        // result : ... item15.member.DefaultMemberService
+        Arrays.stream(PrivateClass.class.getDeclaredFields()).forEach(System.out::println);
+    }
+
 }
