5e6c4a0123269f50bb66fea6e098da9a4c92b09b
simjeongsoo <simking95@gmail.com>
2023-10-21 16:09:33 +0900

    item 10 - equals 대칭성 위배

---
+++ effective-java/src/main/java/item10/EqualsInJava.java
@@ -0,0 +1,28 @@
+package item10;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class EqualsInJava extends Object {
+
+    public static void main(String[] args) throws MalformedURLException {
+        long time = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(time); // date를 상속받은 Timestamp 클래스
+        Date date = new Date(time);
+
+        // 대칭성 위배 예시(p.60)
+//        System.out.println(date.equals(timestamp));
+//        System.out.println(timestamp.equals(date));
+
+        // 일관성 위배 가능성 있음(p.61)
+        URL google1 = new URL("https", "about.google", "/products/");
+        URL google2 = new URL("https", "about.google", "/products/");
+        System.out.println(google1.equals(google2));
+
+
+
+    }
+
+}
